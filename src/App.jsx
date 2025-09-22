f050a322fd3a6f854c19731fc460052ab53f8317
rsmall <stendet@proton.me>
2025-09-23 00:51:01 +0300

    useState stady

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,23 @@ import './App.css'
 function App() {
 
   const [likes, setlikes] = useState(0)
+  const [value, setValue] = useState('')
 
-  function increment {
+  function increment() {
     setlikes(likes + 1)
   };
 
-  function decrement {
+  function decrement() {
     setlikes(likes - 1)
   };
 
   return (
     <div className='App'>
+      <h1>{likes}</h1>
+      <h2>{value}</h2>
+      <input onChange={(e => setValue(e.target.value))} value={value}></input>
       <button onClick={increment}>increment</button>
       <button onClick={decrement}>decrement</button>
-      <div>{likes}</div>
     </div>
   );
 }

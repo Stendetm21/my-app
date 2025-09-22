06f6c9b5bb2bfec36bf582601451eabee3475242
rsmall <stendet@proton.me>
2025-09-23 00:29:35 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [arr, setArr] = useState([])
-  const [str, setStr] = useState('') // состояние input
 
-  const saveName = () => {
-    if (str.trim() === '') return // не добавляем пустую строку
-    setArr([...arr, str]) // добавляем введённый текст в массив
-    setStr('') // очищаем input
-  }
+  const state = useState()
 
+  let likes = 0;
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-
-      {/* просто количество элементов в массиве */}
-      <div>Всего людей: {arr.length}</div>
-
-      <div className="card">
-        <button onClick={() => setCount(count + 1)}>
-          count is {count}
-        </button>
-
-        <button onClick={saveName}>Добавить</button>
-
-        <input
-          type="text"
-          value={str}
-          onChange={(e) => setStr(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') saveName()
-          }}
-          placeholder="Введите имя"
-        />
-      <div className='arrName'>
-        <ul>
-          {arr.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>        
-      </div>
-        <ul>
-          {arr.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      </div>
-    </>
-  )
+    <div className='App'>
+      <button onClick={() => likes += 1}>increment</button>
+      <button onClick={() => likes -= 1}>decrement</button>
+      <div>{likes}</div>
+    </div>
+  );
 }
 
 export default App
